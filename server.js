010b60e98aa4fcad1254a23174c8e4215988bf01
Wasif Zaman <teatocode@gmail.com>
2017-08-25 10:57:22 -0400

    express skeleton

---
+++ server.js
@@ -0,0 +1,23 @@
+// lets do eeet
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const orders = require('./routes/orders');
+
+const app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(orders);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+  console.log(`listening on port ${port}`);
+})
